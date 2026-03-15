06672ca0fa4b9965e878826c8568d5c0d0bef456
Bruno Santos <brunosantos6235@gmail.com>
2026-03-15 18:41:14 -0300

    configura H2 para testes no CI

--- src/test/java/com/corridaapp/corridatreino/CorridaTreinoApplicationTests.java
+++ src/test/java/com/corridaapp/corridatreino/CorridaTreinoApplicationTests.java
@@ -2,12 +2,14 @@ package com.corridaapp.corridatreino;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CorridaTreinoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
